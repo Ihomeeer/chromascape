c2f933cdc25ff00ed714f0f094bee539c9779025
Mikhail Kirichkov <mihailkirichkov@yandex.ru>
2025-02-17 17:43:05 +0300

    forest article in process, testing image mask

--- src/components/ForestArticle/ForestArticle.tsx
+++ src/components/ForestArticle/ForestArticle.tsx
@@ -3,13 +3,32 @@ import styles from './ForestArticle.module.scss';
 
 interface ForestArticleProps {
   text: string;
+  title?: string;
+  imageURL: string;
+  imageAlt: string;
 }
 
-const ForestArticle: React.FC<ForestArticleProps> = ({text}) => {
+const ForestArticle: React.FC<ForestArticleProps> = ({
+  text,
+  title,
+  imageURL,
+  imageAlt,
+}) => {
+  const avifSrc = `${imageURL}.avif`;
+  const jpegSrc = `${imageURL}.jpg`;
   return (
-    <p>
-      {text}
-    </p>
+    <article className={styles.article}>
+      {title && <h3 className={styles.title}>
+        {title}
+      </h3>}
+      <p className={styles.text}>
+        {text}
+      </p>
+      <picture className={styles.pictureContainer}>
+        <source srcSet={avifSrc} type="image/avif" />
+        <img className={styles.picture} src={jpegSrc} alt={imageAlt} />
+      </picture>
+    </article>
   )
 }
 
--- src/components/SectionForest/SectionForest.tsx
+++ src/components/SectionForest/SectionForest.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import styles from './SectionForest.module.scss';
 import { forestHeader } from '../../utils/dataForest';
+import { forestArticle, forestTypes } from "../../utils/dataForest";
 
 import AppHeader from "../AppHeader/AppHeader";
 import AppFooter from "../AppFooter/AppFooter";
+import ForestArticle from "../ForestArticle/ForestArticle";
 
 interface SectiomForestProps {
 
 }
 
 const SectionForest: React.FC<SectiomForestProps> = () => {
+
   return (
     <>
       <AppHeader />
@@ -17,6 +20,9 @@ const SectionForest: React.FC<SectiomForestProps> = () => {
         <div className={styles.headerContainer}>
           <h1 className={styles.header}>{forestHeader}</h1>
         </div>
+        <ForestArticle
+          {...forestArticle}
+        />
 
       </section>
       <AppFooter />
