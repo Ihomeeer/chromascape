9f936652e436f53a1393e20178f81d29f40e807b
Mikhail Kirichkov <mihailkirichkov@yandex.ru>
2025-02-20 17:45:41 +0300

    desert section, react-intersection-observer, framer-motion

--- src/components/AppRouter/AppRouter.tsx
+++ src/components/AppRouter/AppRouter.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from '../MainPage/MainPage';
+import SectionDesert from '../SectionDesert/SectionDesert';
 
 const AppRouter: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainPage />} />
+        <Route path="/desert" element={<SectionDesert />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/utils/dataDesert.tsx
@@ -0,0 +1,36 @@
+export const desertHeader = 'Пустыня: там, где время замедляется';
+
+export const desertPhotos = [
+  {
+    imageURL: '/src/images/forest/deciduous',
+    imageAlt: 'Лиственный лес',
+  },
+  {
+    imageURL: '/src/images/forest/deciduous',
+    imageAlt: 'Лиственный лес',
+  }, {
+    imageURL: '/src/images/forest/deciduous',
+    imageAlt: 'Лиственный лес',
+  }, {
+    imageURL: '/src/images/forest/deciduous',
+    imageAlt: 'Лиственный лес',
+  }, {
+    imageURL: '/src/images/forest/deciduous',
+    imageAlt: 'Лиственный лес',
+  }, {
+    imageURL: '/src/images/forest/deciduous',
+    imageAlt: 'Лиственный лес',
+  }, {
+    imageURL: '/src/images/forest/deciduous',
+    imageAlt: 'Лиственный лес',
+  }, {
+    imageURL: '/src/images/forest/deciduous',
+    imageAlt: 'Лиственный лес',
+  }, {
+    imageURL: '/src/images/forest/deciduous',
+    imageAlt: 'Лиственный лес',
+  }, {
+    imageURL: '/src/images/forest/deciduous',
+    imageAlt: 'Лиственный лес',
+  },
+]
