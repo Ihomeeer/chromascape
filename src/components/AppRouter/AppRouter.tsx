0dfb1f77d7e7eb51899c89986e8464034f9522a6
Ihomeeer <ihomeer@gmail.com>
2025-02-27 21:22:45 +0300

    DesertSection, Router

--- src/components/AppRouter/AppRouter.tsx
+++ src/components/AppRouter/AppRouter.tsx
@@ -1,22 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from '../MainPage/MainPage';
-<<<<<<< HEAD
 import SectionForest from '../SectionForest/SectionForest';
-=======
 import SectionDesert from '../SectionDesert/SectionDesert';
->>>>>>> 9f936652e436f53a1393e20178f81d29f40e807b
 
 const AppRouter: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainPage />} />
-<<<<<<< HEAD
         <Route path="/forest" element={<SectionForest />} />
-=======
         <Route path="/desert" element={<SectionDesert />} />
->>>>>>> 9f936652e436f53a1393e20178f81d29f40e807b
       </Routes>
     </BrowserRouter>
   );
